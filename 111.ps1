1f510e4e003ca5aeb68dc658dc3731cf467488a3
wenty <2321901849@qq.com>
2025-10-22 17:41:42 +0800

    okokok

--- 111.ps1
+++ 111.ps1
@@ -1,7 +1,7 @@
 $urls = @(
-  "https://mirror.msys2.org/msys/i686/dtc-1.7.2-1-i686.pkg.tar.zst",
-  "https://mirror.msys2.org/msys/i686/gcc-libs-13.3.0-1-i686.pkg.tar.zst",
-  "https://mirror.msys2.org/msys/i686/libyaml-0.2.5-2-i686.pkg.tar.zst",
+  "https://repo.msys2.org/msys/x86_64/gperf-3.1-6-x86_64.pkg.tar.zst",
+  "https://repo.msys2.org/msys/i686/gperf-3.1-6-i686.pkg.tar.zst",
+  "https://mirror.msys2.org/msys/i686/msys2-runtime-3.3-3.3.6-13-i686.pkg.tar.zst",
   "https://repo.msys2.org/msys/i686/msys2-runtime-3.3-3.3.6-13-i686.pkg.tar.zst"
 )
 foreach ($u in $urls) {
